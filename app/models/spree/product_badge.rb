cbe7070db922128bcd624963dc51fc562eb85e55
monika2110 <monikakrizanac21@gmail.com>
2022-07-03 16:43:14 +0200

    fixed product_badge model

--- app/models/spree/product_badge.rb
+++ app/models/spree/product_badge.rb
@@ -2,6 +2,6 @@ module Spree
   class ProductBadge < ApplicationRecord
     belongs_to :product, class_name: 'Spree::Product'
     belongs_to :badge, class_name: 'Spree::Badge'
-    validates :product, uniqueness: { scope: :product }
+    validates :product, uniqueness: { scope: :badge }
   end
 end
